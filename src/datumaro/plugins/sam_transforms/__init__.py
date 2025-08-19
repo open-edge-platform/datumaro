49158a4c4546fa227ba627ff30dadad0bfb6c266
Grégoire Payen de La Garanderie <gregoire.payen.de.la.garanderie@intel.com>
2025-08-19 14:55:38 +0100

    Cleanup comment

--- src/datumaro/plugins/sam_transforms/__init__.py
+++ src/datumaro/plugins/sam_transforms/__init__.py
@@ -3,5 +3,4 @@
 # SPDX-License-Identifier: MIT
 """Transforms using Segment Anything Model"""
 
-# Note: SAM transforms have been removed as they depended on the deprecated ModelTransform class
 __all__ = []
