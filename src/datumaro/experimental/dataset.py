5e712f22323a680c743c228a25d3a02e363fc1f9
Albert van Houten <albert.van.houten@intel.com>
2025-08-20 15:52:47 +0200

    Remove comment

--- src/datumaro/experimental/dataset.py
+++ src/datumaro/experimental/dataset.py
@@ -304,9 +304,6 @@ class Dataset(Generic[DType]):
         Returns:
             A new Dataset instance with the converted schema
         """
-        # Import the converter implementations to register them
-        # ruff: noqa: F401
-
         # Determine target schema
         if isinstance(target_dtype_or_schema, Schema):
             target_schema = target_dtype_or_schema
