87e51a2bf21271c81d3369c5c4949006312e4394
Jort Bergfeld <jort.bergfeld@intel.com>
2025-11-14 10:20:33 +0100

    Reduce number of imports in init
    Signed-off-by: Jort Bergfeld <jort.bergfeld@intel.com>

--- src/datumaro/experimental/__init__.py
+++ src/datumaro/experimental/__init__.py
@@ -2,37 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from .converters import ConverterRegistry, converter
 from .dataset import Dataset, Sample
 from .export_import import export_dataset, import_dataset
 from .fields import Field, Semantic
-from .fields.annotations import (
-    BBoxField,
-    LabelField,
-    PolygonField,
-    RotatedBBoxField,
-    ScoreField,
-    bbox_field,
-    label_field,
-    polygon_field,
-    rotated_bbox_field,
-    score_field,
-)
-from .fields.images import (
-    ImageCallableField,
-    ImageField,
-    ImageInfo,
-    ImageInfoField,
-    ImagePathField,
-    TensorField,
-    image_callable_field,
-    image_field,
-    image_info_field,
-    image_path_field,
-    tensor_field,
-)
 from .schema import AttributeInfo, Schema
-
-# Import tilers and converters implementations to register them
-from .tiling import tilers
-from .type_registry import register_from_polars_converter, register_numpy_converter
